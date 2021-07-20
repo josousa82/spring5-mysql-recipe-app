3b10fa24c4bd70c59e14c47968c802912e193657
josousa82 <mfgas2@ou.ac.uk>
2021-07-21 00:56:15 +0100

    added production db and scripts WIP

---
+++ support-containers/mysql/schemas/02-uom-data.sql
@@ -0,0 +1,21 @@
+USE recipe_app_prod;
+
+INSERT INTO unit_of_measure (description) VALUES ('Teaspoon');
+INSERT INTO unit_of_measure (description) VALUES ('Tablespoon');
+INSERT INTO unit_of_measure (description) VALUES ('Cup');
+INSERT INTO unit_of_measure (description) VALUES ('Pinch');
+INSERT INTO unit_of_measure (description) VALUES ('Ounce');
+INSERT INTO unit_of_measure (description) VALUES ('Each');
+INSERT INTO unit_of_measure (description) VALUES ('Dash');
+INSERT INTO unit_of_measure (description) VALUES ('Pint');
+
+# USE recipe_app_dev;
+#
+# INSERT INTO unit_of_measure (description) VALUES ('Teaspoon');
+# INSERT INTO unit_of_measure (description) VALUES ('Tablespoon');
+# INSERT INTO unit_of_measure (description) VALUES ('Cup');
+# INSERT INTO unit_of_measure (description) VALUES ('Pinch');
+# INSERT INTO unit_of_measure (description) VALUES ('Ounce');
+# INSERT INTO unit_of_measure (description) VALUES ('Each');
+# INSERT INTO unit_of_measure (description) VALUES ('Dash');
+# INSERT INTO unit_of_measure (description) VALUES ('Pint');
